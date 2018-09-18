967d61e8524e5dd8f83309019e53b0b627b9b965
jimhantrix <jimtekanyo@gmail.com>
2018-09-18 16:23:27 +0800

    created an index page

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+	
+	render() {
+
+	return(
+
+			<div>
+				<div> CountDdown to December 25, 2017 </div>
+
+				<div>
+					<div> 14 Days </div>
+					<div> 30 hours</div>
+					<div> 15 minutes</div>
+					<div> 20 seconds</div>
+				</div>
+
+				<div>
+
+					<input placeholder='new date' />
+
+					<button>Submit</button> 
+
+				</div>
+			</div>
+		)
+
+	}
+}
+export default App;
