ca4fc494657105c902d9e8f65dcad6b7374ed23e
jimhantrix <jimtekanyo@gmail.com>
2018-09-18 16:52:30 +0800

    added new class names

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,16 @@ class App extends Component {
 	render() {
 
 	return(
+			<div className='App'>
 
-			<div>
-				<div> CountDdown to December 25, 2018 </div>
+			
+				<div className='App-title'> Count down to December 25, 2018 </div>
 
 				<div>
-					<div> 14 Days </div>
-					<div> 30 hours</div>
-					<div> 15 minutes</div>
-					<div> 20 seconds</div>
+					<div className="Clock-days"> 14 Days </div>
+					<div className="Clock-hours"> 30 hours</div>
+					<div className="Clock-Minutes"> 15 minutes</div>
+					<div className="Clock-seconds"> 20 seconds</div>
 				</div>
 
 				<div>
